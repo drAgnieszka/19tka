04d339f4287e42d77ff81a59331c7f9a2d3f9e91
Agnieszka <agnieszka@if.pw.edu.pl>
2024-05-25 21:45:06 +0200

    move measurments to data folder

--- measure_temp_hum.sh
+++ measure_temp_hum.sh
@@ -24,9 +24,9 @@ temp=$(echo "$data" | sed 's/.*, \(.*\))/\1/')
 #echo "Humidity: $hum"
 
 # check if file exist and create it if not
-if [ ! -f "$current_day.csv" ]; then
-	touch "$current_day.csv"
-	echo "date,time,temperature,humidity" >> "$current_day.csv"
+if [ ! -f "data/$current_day.csv" ]; then
+	touch "data/$current_day.csv"
+	echo "date,time,temperature,humidity" >> "data/$current_day.csv"
 fi
 
-echo "$current_day,$current_time,$temp,$hum" >> "$current_day.csv"
+echo "$current_day,$current_time,$temp,$hum" >> "data/$current_day.csv"
